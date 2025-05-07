ef4c38e7e31cee355ad2df32c0056b5b48c73b7f
johnstontu <tuckerjohnston13@gmail.com>
2025-05-06 20:37:09 -0400

    chapter 1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/johnstontu/Chirpy
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func main() {
+	const port = "8080"
+	const filepathRoot = "."
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handler)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
+}
